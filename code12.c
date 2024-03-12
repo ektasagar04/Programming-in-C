67ca55bac9afc7288daa9e208c3fe0b2dbec211e
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-12 23:29:36 +0530

    Add files via upload

---
+++ code12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+//calculate simple interest and compound interest
+int main()
+{
+    char a;
+    printf("enter a character:\n");
+    scanf("%c", &a);
+    printf("ASCII value of %c: %d\n", a,a);
+    return 0;
+}
