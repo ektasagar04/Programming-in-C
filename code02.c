280aec3ee621eae9b55a4293e1ced51cd380e17a
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-12 20:52:44 +0530

    Add files via upload

---
+++ code02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int age = 25;
+    printf("age is %d\n", age);
+    char star = '*';
+    printf("star is %c\n", star);
+    int number = 22;
+    printf("number is %d\n", number);
+    float pi = 3.14;
+    printf("pi is %f\n", pi);
+    
+    return 0;
+}
