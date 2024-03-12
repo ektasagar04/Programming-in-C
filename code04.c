8410f51f8fd04c3b4aa0cf0e3edb2f48572edda0
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-12 20:53:21 +0530

    Add files via upload

---
+++ code04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter a:\n");
+    scanf("%d", &a);
+    printf("enter b:\n");
+    scanf("%d", &b);
+    printf("sum is : %d\n", a+b);
+    printf("sub is : %d\n", a-b);
+    printf("multi is : %d\n", a*b);
+    printf("div is : %d\n", a/b);
+    printf("mod is : %d\n", a%b);
+return 0;
+}
