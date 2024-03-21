da9ce9bfb333aa3cfab8d5d7bbbd484ee5ab32a3
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-22 00:45:12 +0530

    Add files via upload

---
+++ Question03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+// addition of two decimals
+{
+    float a,b;
+    printf("enter a:\n");
+    scanf("%f", &a);
+    printf("enter b:\n");
+    scanf("%f", &b);
+    printf("sum is : %f",a + b);
+    return 0;
+}
