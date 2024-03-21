4b416889783dd159d320b368d88b4159ad81016d
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-22 00:46:16 +0530

    Add files via upload

---
+++ Question05.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+// print ASCII vale of character
+{
+    char a;
+    printf("Enter a charcter:\n");
+    scanf("%c", &a);
+    printf("print ASCII of %c: %d\n",a,a);
+    return 0;
+}
