793b99ed2deae0232aa2ff9f4d9f5010559ef990
Ekta Sagar <147904656+ektasagar04@users.noreply.github.com>
2024-03-22 00:44:44 +0530

    Add files via upload

---
+++ Question02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+// addition of two integers
+{
+    int a,b;
+    printf("enter a:\n");
+    scanf("%d", &a);
+    printf("enter b:\n");
+    scanf("%d", &b);
+    printf("sum is : %d", a+b);
+    return 0;
+}
